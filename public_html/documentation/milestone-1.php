9ab6111a7b321585c174378a2b085018aeb84bfc
Anthony Williams <pull.rebase>
2016-04-19 16:19:56 -0600

    Recreated williams-devops-website

---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+      <title>PWP-MileStone-1</title>
+	</head>
+	<body>
+		<header>Persona</header>
+		<h1>Chief Executive Officer/founder of hedge fund</h1>
+		<p>Name: Johnny J. Johnson</p>
+		<p>Age: 45</p>
+	</body>
+		<h1>Project 1 PWP milestone 1</h1>
+
+	   <p>Johnny Johnson, is seeking an experienced, motivated front-end software developer to join our team. Our product is unique, so this is a greenfield opportunity. </p>
+
+		<p>You will work on the redevelopment of Street Diligence’s core application, which has just undergone a redesign. You will play a pivotal role in defining our website </p>
+
+		<p>We’re looking for a business-minded web developer with superior skills in JavaScript and a solid knowledge base of back-end technologies. You will challenge us to think about things differently.</p>
+
+		<p>Key Responsibilities:
+			<br />• Work with the product development team to create graceful &innovative solutions
+			<br />• Create and implement powerful, reusable and extensible JavaScript frameworks
+			<br />• Work with other developers to integrate the front and back-end
+			<br />• Prepare cross-browser/platform JavaScript code for browsers
+			<br />• Take a design prototype and integrate it with back-end/server side functionality</p>
+
+		<p>About you:
+			<br />• Excited about working at a fintech startup &bring innovative software to market
+			<br />• Ability to write JavaScript, HTML and CSS
+			<br />• Experience with server-side scripting, such as PHP, JavaScript, and JSON
+			<br />• Demonstrated experience wiring JavaScript/HTML/CSS into full-blown interactive apps
+			<br />• Experience with Object-Oriented Programming languages (e.g. PHP, JavaScript, and SQL</p>
