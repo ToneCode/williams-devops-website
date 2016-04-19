0caa53e40f703fbfe22ab752f11928bcc28d7c52
Anthony Williams <pull.rebase>
2016-04-19 16:23:41 -0600

    Recreated williams-devops-website

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -2,32 +2,53 @@
 <html lang="en">
 	<head>
 		<meta charset="UTF-8">
-      <title>PWP-MileStone-1</title>
+		<title>PWP-MileStone-1</title>
 	</head>
 	<body>
 		<header>Persona</header>
-		<h1>Chief Executive Officer/founder of hedge fund</h1>
+		<h1>Chief Executive Officer/founder of hedge fund!</h1>
 		<p>Name: Johnny J. Johnson</p>
 		<p>Age: 45</p>
-	</body>
+
 		<h1>Project 1 PWP milestone 1</h1>
 
-	   <p>Johnny Johnson, is seeking an experienced, motivated front-end software developer to join our team. Our product is unique, so this is a greenfield opportunity. </p>
+		<p>Johnny Johnson, is seeking an experienced, motivated Full Stack software developer to join his team. His product is unique, so this is a greenfield opportunity. </p>
 
-		<p>You will work on the redevelopment of Street Diligence’s core application, which has just undergone a redesign. You will play a pivotal role in defining our website </p>
+		<p>You will work on the redevelopment of Street Diligence’s core application, which has just undergone a redesign. You will play a pivotal role in defining our website. </p>
 
-		<p>We’re looking for a business-minded web developer with superior skills in JavaScript and a solid knowledge base of back-end technologies. You will challenge us to think about things differently.</p>
+		<p>We’re looking for a web developer with superior skills in JavaScript and a solid knowledge base of back-end technologies. You will challenge us with clean code.</p>
 
-		<p>Key Responsibilities:
-			<br />• Work with the product development team to create graceful &innovative solutions
-			<br />• Create and implement powerful, reusable and extensible JavaScript frameworks
-			<br />• Work with other developers to integrate the front and back-end
-			<br />• Prepare cross-browser/platform JavaScript code for browsers
-			<br />• Take a design prototype and integrate it with back-end/server side functionality</p>
+		<p>Key Responsibilities:</p>
+		<ul>
+			<li> Work with the product development team to create graceful and innovative solutions</li>
+			<li> Create and implement powerful, reusable and extensible JavaScript frameworks</li>
+			<li> Work with other developers to integrate the front and back-end</li>
+			<li> Prepare cross-browser/platform JavaScript code for browsers</li>
+			<li> Take a design prototype and integrate it with back-end/server side functionality</li>
+		</ul>
+		<p>About you:</p>
+		<ul>
+			<li> Excited about working at a fintech startup, help bring innovative software to market</li>
+			<li> Ability to write JavaScript, HTML and CSS</li>
+			<li> Experience with server-side scripting, such as PHP, JavaScript, and JSON</li>
+			<li> Demonstrated experience wiring JavaScript/HTML/CSS into full-blown interactive apps</li>
+			<li> Experience with Object-Oriented Programming languages (e.g. PHP, JavaScript, and SQL)</li>
+		</ul>
 
-		<p>About you:
-			<br />• Excited about working at a fintech startup &bring innovative software to market
-			<br />• Ability to write JavaScript, HTML and CSS
-			<br />• Experience with server-side scripting, such as PHP, JavaScript, and JSON
-			<br />• Demonstrated experience wiring JavaScript/HTML/CSS into full-blown interactive apps
-			<br />• Experience with Object-Oriented Programming languages (e.g. PHP, JavaScript, and SQL</p>
+		<h2>Attitudes and Behaviors:</h2>
+		`	<p>Johnny works in a high stress environment, therefore has limited time to view potential candidates sites. Currently seeking prospects through high tech and
+			entertainment industry networking and social events.  Johnny is very active on social media, blog sites, LinkedIn and Twitter.</p>
+
+		<h2>Frustrations and Needs:</h2>
+		<p>Johnny needs to hire someone with top tier skills, do to losing his Sr. Web Developer, who left for a higher salary.  This potential candidate needs to start Asap!
+			Salary is negotiable and based on experience.</p>
+
+		<h2>Goals:</h2>
+		<p>Johnny has been looking at potential candidates web sites and viewing their experience on LinkedIn, with his limited time he only spends 30 to 60 seconds to see
+			weather or not the potential candidate has the minimum qualifications to warrant a call.</p>
+
+		<h2>Use Case:</h2>
+		<p>CEO and Founder has done a brief look at my personal web site for more information.  He has viewed my LinkeIn profile and decided to schedule a 15 minute phone
+			screening call, to see if I would be a good fit the this position.</p>
+	</body>
+</html>
