ac43a7c3595afea908f5a4c713a5dac21b549af1
Anthony Williams <pull.rebase>
2016-04-19 23:47:31 -0600

    Added content to site

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -6,49 +6,39 @@
 	</head>
 	<body>
 		<header>Persona</header>
-		<h1>Chief Executive Officer/founder of hedge fund!</h1>
-		<p>Name: Johnny J. Johnson</p>
-		<p>Age: 45</p>
 
-		<h1>Project 1 PWP milestone 1</h1>
-
-		<p>Johnny Johnson, is seeking an experienced, motivated Full Stack software developer to join his team. His product is unique, so this is a greenfield opportunity. </p>
-
-		<p>You will work on the redevelopment of Street Diligence’s core application, which has just undergone a redesign. You will play a pivotal role in defining our website. </p>
-
-		<p>We’re looking for a web developer with superior skills in JavaScript and a solid knowledge base of back-end technologies. You will challenge us with clean code.</p>
-
-		<p>Key Responsibilities:</p>
+		<h2>Job Title and Major Responsibilities</h2>
+		<p>Chief Information Officer</p>
 		<ul>
-			<li> Work with the product development team to create graceful and innovative solutions</li>
-			<li> Create and implement powerful, reusable and extensible JavaScript frameworks</li>
-			<li> Work with other developers to integrate the front and back-end</li>
-			<li> Prepare cross-browser/platform JavaScript code for browsers</li>
-			<li> Take a design prototype and integrate it with back-end/server side functionality</li>
+		<li>Name: Matthew Findley</li>
+		<li>Age: 51</li>
+		<li>Married</li>
+		<li>Father of three children</li>
+		<li>Has a MBA in Business and Ph.D. in Electrical Engineering.</li>
 		</ul>
-		<p>About you:</p>
+		<p>Matthew is focused, goal-oriented with a strong leadership role. One of his
+			concerns is maintaining quality across all groups under his management.  He
+			spends the majority of his time:</p>
 		<ul>
-			<li> Excited about working at a fintech startup, help bring innovative software to market</li>
-			<li> Ability to write JavaScript, HTML and CSS</li>
-			<li> Experience with server-side scripting, such as PHP, JavaScript, and JSON</li>
-			<li> Demonstrated experience wiring JavaScript/HTML/CSS into full-blown interactive apps</li>
-			<li> Experience with Object-Oriented Programming languages (e.g. PHP, JavaScript, and SQL)</li>
+		<li>Requesting and reviewing research reports.</li>
+		<li>Preparing memos and briefs for all agency heads.</li>
+		<li>Managing cross sectional teams within the organization.</li>
 		</ul>
-
-		<h2>Attitudes and Behaviors:</h2>
-		`	<p>Johnny works in a high stress environment, therefore has limited time to view potential candidates sites. Currently seeking prospects through high tech and
-			entertainment industry networking and social events.  Johnny is very active on social media, blog sites, LinkedIn and Twitter.</p>
-
-		<h2>Frustrations and Needs:</h2>
-		<p>Johnny needs to hire someone with top tier skills, do to losing his Sr. Web Developer, who left for a higher salary.  This potential candidate needs to start Asap!
-			Salary is negotiable and based on experience.</p>
-
-		<h2>Goals:</h2>
-		<p>Johnny has been looking at potential candidates web sites and viewing their experience on LinkedIn, with his limited time he only spends 30 to 60 seconds to see
-			weather or not the potential candidate has the minimum qualifications to warrant a call.</p>
-
+		<h2>Environment</h2>
+		<p>He has a custom built 27" MacBook pro desktop and a 13" MacBook Air laptop.  Uses
+			the internet several hours daily for work and utilizes an iPad exclusively to read books.
+			He uses social media to communicate information about the company on FaceBook and LinkedIn.</p>
+		<h2>Frustrations</h2>
+		<p>Most days, product managers spent significant time with the developers in formal meetings or
+			ad-hoc discoussions championing, designing and re-scoping features. While the developers have
+			strong opinions on what was best for their product, as well as what was possible.  The developers
+			often expressed frustration that their feature ideas were not reflected in the requirements.</p>
+		<h2>Goals</h2>
+		<p>To Hire someone with Continuous Integration and Continuous Deployment skills, to not only reduce
+			risks and catch bugs quickly, but also move rapidly to working software.  This allows for greater
+			collaboration between ops and delivery.</p>
 		<h2>Use Case:</h2>
-		<p>CEO and Founder has done a brief look at my personal web site for more information.  He has viewed my LinkeIn profile and decided to schedule a 15 minute phone
-			screening call, to see if I would be a good fit the this position.</p>
+		<p>Matthew has done a brief look at my personal web site for more information.  He has also viewed my LinkedIn
+			profile and decided to schedule a 15 minute phone screening call, to see if I would be a good fit to join his team.</p>
 	</body>
 </html>
