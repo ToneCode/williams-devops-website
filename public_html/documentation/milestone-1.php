cbfe7ff39728b5ae8a7072a532999420915a8cf3
Anthony Williams <pull.rebase>
2016-04-26 21:12:25 -0600

    Added purpose, audience & goal to the site

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -6,19 +6,19 @@
 	</head>
 	<body>
 		<h1>Purpose, Audience & Goal</h1>
-		<h2>Purpose:</h2>
+		<h3>Purpose:</h3>
 		<p>This project is a professional portfolio website with a focus on front-end development.</p>
-		<h2>Audience:</h2>
+		<h3>Audience:</h3>
 		<p>This site is designed with a professional audience in mind, and is targeted to Fortune 1000 companies
 			nationwide looking to add personnel to their web development departments.</p>
-		<h2>Goal:</h2>
+		<h3>Goal:</h3>
 		<p>The primary goal of this site is for users to initiate contact with me in regards to employment and possible
 			collaboration opportunities, using connected social media profiles and contact information.  The secondary
 			goal is to contribute to a polished professional online presence and to develop a personal brand in the web
 			development community.</p>
 		<header>Persona</header>
 
-		<h2>Job Title and Major Responsibilities</h2>
+		<h3>Job Title and Major Responsibilities</h3>
 		<p>Chief Information Officer</p>
 		<ul>
 		<li>Name: Matthew Findley</li>
@@ -35,20 +35,20 @@
 		<li>Preparing memos and briefs for all agency heads.</li>
 		<li>Managing cross sectional teams within the organization.</li>
 		</ul>
-		<h2>Environment</h2>
+		<h3>Environment</h3>
 		<p>He has a custom built 27" MacBook pro desktop and a 13" MacBook Air laptop.  Uses
 			the internet several hours daily for work and utilizes an iPad exclusively to read books.
 			He uses social media to communicate information about the company on FaceBook and LinkedIn.</p>
-		<h2>Frustrations</h2>
+		<h3>Frustrations</h3>
 		<p>Most days, product managers spend significant time with the developers in formal meetings or
 			ad-hoc discussions championing, designing and re-scoping features. The developers have also found
 			that the need for continuous automation methodology can help them automate the entire build process.
 			</p>
-		<h2>Goals</h2>
+		<h3>Goals</h3>
 		<p>To Hire someone with Continuous Integration and Continuous Deployment skills, to not only reduce
 			risks and catch bugs quickly, but also move rapidly toward working software.  This allows for greater
 			collaboration between ops and delivery.</p>
-		<h2>Use Case:</h2>
+		<h3>Use Case:</h3>
 		<p>Matthew has received a cover letter and resume from myself in regards to an open position for a DevOps Specialist.
 			Based on a brief look at my resume, he has included my name on a list of potential people to search for on
 			LinkedIn.  After viewing my LinkedIn profile, he noticed my training in Puppet Labs and RedHat Open Stack,
