634dd0171d3c01a1816d9508402c76b6839da5f4
Anthony Williams <pull.rebase>
2016-04-30 16:02:31 -0600

    Updated Matthew

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -19,10 +19,10 @@
 		<p><strong>Contact:</strong>This page will contain a simple contact form, my professional email address, and links to my professional social media profiles: LinkedIn, Twitter and GitHub. These will also be linked in the footer site wide via icons. This page may be integrated into my About Me page if content is short. </p>
 		</div>
 		<h3>Interaction Flow</h3>
-		<p>David is viewing the site on his desktop computer at the office, with a 27" screen, using Safari. He has 29 other tabs open. From LinkedIn, he links to my personal website which opens in a new window. In the next 3 minutes, here is an outline of David's interactions with my site: </p>
+		<p>Matthew is viewing the site on his desktop computer at the office, with a 27" screen, using Safari. He has 29 other tabs open. From LinkedIn, he links to my personal website which opens in a new window. In the next 3 minutes, here is an outline of Matthew's interactions with my site: </p>
 		<ol>
 			<li>Lands on Home page. Views a "quote where I describe my work flow" Clicks call to action that navigates to the About Me page.</li>
-			<li>Views my core skills featured in a contrasting side panel.  Finds my professional history to be outstanding.  The interaction stops there.  David has decided, based on the information provide, that I will be included in the preliminary phone screens.  He does not need to navigate away, because my contact info is visible in the footer.  David leaves the About Me page open for reference during the prelimary phone call.</li>
+			<li>Views my core skills featured in a contrasting side panel.  Finds my professional history to be outstanding.  The interaction stops there.  Matthew has decided, based on the information provided, that I will be included in the preliminary phone screens.  He does not need to navigate away, because my contact info is visible in the footer.  Matthew leaves the About Me page open for reference during the preliminary phone call.</li>
 		</ol>
 		<h3>WireFrames</h3>
 		<p>This site will feature 2 layouts for :</p>
