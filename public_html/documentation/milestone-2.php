7434a9d825a3b143316a879ca0736a3fd4aee172
Anthony Williams <pull.rebase>
2016-04-30 18:48:52 -0600

    Corrected home page mobile layout

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -29,6 +29,7 @@
 		<ol>
 			<li>Home Page Layout</li>
 			<img src="images/home-page-desktop.svg" alt="Desktop WireFrame"
+			<li>Home Page Mobile Layout</li>		  
 			<img src="images/home-page-mobile.svg" alt="Mobile WireFrame"	  
 			<li>Content Page Layout</li>
 
