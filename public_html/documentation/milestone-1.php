771e65a0cea6f77da0359fc474bf71f93c406b72
Anthony Williams <mylifeassurance@aol.com>
2016-05-23 11:00:30 -0600

    Added formatting

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -13,7 +13,7 @@
 			nationwide looking to add personnel to their web development departments.</p>
 		<h3>Goal:</h3>
 		<p>The primary goal of this site is for users to initiate contact with me in regards to employment and possible
-			collaboration opportunities, using connected social media profiles and contact information.  The secondary
+			collaboration opportunities, using connected social media profiles and contact information. The secondary
 			goal is to contribute to a polished professional online presence and to develop a personal brand in the web
 			development community.</p>
 		<header>Persona</header>
@@ -21,37 +21,38 @@
 		<h3>Job Title and Major Responsibilities</h3>
 		<p>Hiring Authority</p>
 		<ul>
-		<li>Name: Matthew Findley</li>
-		<li>Age: 51</li>
-		<li>Married</li>
-		<li>Father of three children</li>
-		<li>Has an MBA in Business and Ph.D. in Electrical Engineering.</li>
+			<li>Name: Matthew Findley</li>
+			<li>Age: 51</li>
+			<li>Married</li>
+			<li>Father of three children</li>
+			<li>Has an MBA in Business and Ph.D. in Electrical Engineering.</li>
 		</ul>
 		<p>Matthew is focused, goal-oriented with a strong leadership role. One of his
-			concerns is maintaining quality across all groups under his management.  He
+			concerns is maintaining quality across all groups under his management. He
 			spends the majority of his time:</p>
 		<ul>
-		<li>Requesting and reviewing research reports.</li>
-		<li>Preparing memos and briefs for all agency heads.</li>
-		<li>Managing cross sectional teams within the organization.</li>
+			<li>Requesting and reviewing research reports.</li>
+			<li>Preparing memos and briefs for all agency heads.</li>
+			<li>Managing cross sectional teams within the organization.</li>
 		</ul>
 		<h3>Environment</h3>
-		<p>He has a custom built 27" MacBook pro desktop and a 13" MacBook Air laptop.  Uses
+		<p>He has a custom built 27" MacBook pro desktop and a 13" MacBook Air laptop. Uses
 			the internet several hours daily for work and utilizes an iPad exclusively to read books.
 			He uses social media to communicate information about the company on FaceBook and LinkedIn.</p>
 		<h3>Frustrations</h3>
 		<p>Most days, product managers spend significant time with the developers in formal meetings or
 			ad-hoc discussions championing, designing and re-scoping features. The developers have also found
 			that the need for continuous automation methodology can help them automate the entire build process.
-			</p>
+		</p>
 		<h3>Goals</h3>
 		<p>To Hire someone with Continuous Integration and Continuous Deployment training, to not only reduce
-			risks and catch bugs quickly, but also move rapidly toward working software.  This allows for greater
+			risks and catch bugs quickly, but also move rapidly toward working software. This allows for greater
 			collaboration between ops and delivery.</p>
 		<h3>Use Case:</h3>
-		<p>Matthew has received a cover letter and resume from myself in regards to an open position for a DevOps Specialist.
+		<p>Matthew has received a cover letter and resume from myself in regards to an open position for a DevOps
+			Specialist.
 			Based on a brief look at my resume, he has included my name on a list of potential people to search for on
-			LinkedIn.  After viewing my LinkedIn profile, he noticed my training in Puppet Labs and RedHat Open Stack,
+			LinkedIn. After viewing my LinkedIn profile, he noticed my training in Puppet Labs and RedHat Open Stack,
 			he decided to go a step further and visit my web site for more information.</p>
 	</body>
 </html>
