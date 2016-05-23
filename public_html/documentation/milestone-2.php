5b00eea9535fedce9f447aaffd78dc8de4bb6b5e
Anthony Williams <mylifeassurance@aol.com>
2016-05-23 10:59:27 -0600

    Added Hiring Authority

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -1,40 +1,57 @@
 <!DOCTYPE html>
 <html lang="en">
-		<meta charset="UTF-8">
+	<meta charset="UTF-8">
 	<head>
 		<title>PWP-MileStone-2a</title>
 	</head>
 	<body>
 		<h3>Proposed Site Map</h3>
-		<p>Site will consist of 3 top-level pages total. No sub-pages or sub-navigation. This will be the Minimum Value Product.</p>
+		<p>Site will consist of 3 top-level pages total. No sub-pages or sub-navigation. This will be the Minimum Value
+			Product.</p>
 		<ul>
 			<li>Home Page</li>
 			<li>About Me</li>
 			<li>Contact</li>
 		</ul>
 		<div>
-		<h3>Content Strategy</h3>
-		<p><strong>Home Page:</strong>A brief introduction that clearly states my name and role as a Full Stack Developer, and will feature 2 calls to action that will direct the user to the About Me page or Contact page. </p>
-		<p><strong>About Me:</strong>This page will detail my professional history, core skill set, and professional highlights. This page will showcase my strengths and personality. I will feature a contrasting side bar that list my core skills which will be easily readable at a glance. The tone of voice will be casual and personable, indicating that I am only interested in opportunities with organizations that I am a good personality fit. </p>
-		<p><strong>Contact:</strong>This page will contain a simple contact form, my professional email address, and links to my professional social media profiles: LinkedIn, Twitter and GitHub. These will also be linked in the footer site wide via icons. This page may be integrated into my About Me page if content is short. </p>
+			<h3>Content Strategy</h3>
+			<p><strong>Home Page:</strong>A brief introduction that clearly states my name and role as a Full Stack
+				Developer, and will feature 2 calls to action that will direct the user to the About Me page or Contact
+				page. </p>
+			<p><strong>About Me:</strong>This page will detail my professional history, core skill set, and professional
+				highlights. This page will showcase my strengths and personality. I will feature a contrasting side bar that
+				list my core skills which will be easily readable at a glance. The tone of voice will be casual and
+				personable, indicating that I am only interested in opportunities with organizations that I am a good
+				personality fit. </p>
+			<p><strong>Contact:</strong>This page will contain a simple contact form, my professional email address, and
+				links to my professional social media profiles: LinkedIn, Twitter and GitHub. These will also be linked in
+				the footer site wide via icons. This page may be integrated into my About Me page if content is short. </p>
 		</div>
 		<h3>Interaction Flow</h3>
-		<p>Matthew is viewing the site on his desktop computer at the office, with a 27" screen, using Safari. He has 29 other tabs open. From LinkedIn, he links to my personal website which opens in a new window. In the next 3 minutes, here is an outline of Matthew's interactions with my site: </p>
+		<p>Matthew is viewing the site on his desktop computer at the office, with a 27" screen, using Safari. He has 29
+			other tabs open. From LinkedIn, he links to my personal website which opens in a new window. In the next 3
+			minutes, here is an outline of Matthew's interactions with my site: </p>
 		<ol>
-			<li>Lands on Home page. Views a "quote where I describe my work flow" Clicks call to action that navigates to the About Me page.</li>
-			<li>Views my core skills featured in a contrasting side panel.  Finds my professional history to be outstanding.  The interaction stops there.  Matthew has decided, based on the information provided, that I will be included in the preliminary phone screens.  He does not need to navigate away, because my contact info is visible in the footer.  Matthew leaves the About Me page open for reference during the preliminary phone call.</li>
+			<li>Lands on Home page. Views a "quote where I describe my work flow" Clicks call to action that navigates to
+				the About Me page.
+			</li>
+			<li>Views my core skills featured in a contrasting side panel. Finds my professional history to be outstanding.
+				The interaction stops there. Matthew has decided, based on the information provided, that I will be included
+				in the preliminary phone screens. He does not need to navigate away, because my contact info is visible in
+				the footer. Matthew leaves the About Me page open for reference during the preliminary phone call.
+			</li>
 		</ol>
 		<h3>WireFrames</h3>
 		<p>This site will feature 2 layouts for :</p>
 		<h4>Home Page Layout </h4>
 		<img src="images/home-page-desktop.svg" alt="Desktop WireFrame"
-		<br>	  
+		<br>
 		<h4>Home Page Mobile Layout </h4>
 		<img src="images/home-page-mobile.svg" alt="Mobile WireFrame"
 		<br>
 		<h4>Content Page Layout </h4>
 		<img src="images/content-page-desktop.svg" alt="Desktop WireFrame"
-		<br>		  
+		<br>
 		<h4>Content Page Mobile Layout </h4>
 		<img src="images/content-page-mobile.svg" alt="Mobile WireFrame"
 	</body>
