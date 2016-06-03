2684a7623c48fbba6309fc1047537e0229773b19
Anthony Williams <mylifeassurance@aol.com>
2016-06-03 14:51:21 -0600

    added font awesome

--- public_html/php/partials/footer.php
+++ public_html/php/partials/footer.php
@@ -2,6 +2,8 @@
 	<!-- Site footer -->
 	<footer>
 		<p>&copy; 2016 Williams DevOps, Inc.</p>
+		<div class="navbar-text pull-right">
+		<a href="#"><i class="fa fa facebook-square"></i></a></div>
 	</footer>
 	<!-- /container -->
 </div>
