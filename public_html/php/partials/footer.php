db88e5303380c3d797e91a3575cf6e23e8932d1f
Anthony Williams <mylifeassurance@aol.com>
2016-06-03 14:44:41 -0600

    added font awesome

--- public_html/php/partials/footer.php
+++ public_html/php/partials/footer.php
@@ -2,7 +2,6 @@
 	<!-- Site footer -->
 	<footer>
 		<p>&copy; 2016 Williams DevOps, Inc.</p>
-		<img src="../../img/github-logo1.png" alt="GitHub Logo">
 	</footer>
 	<!-- /container -->
 </div>
--- public_html/php/partials/head-utils.php
+++ public_html/php/partials/head-utils.php
@@ -26,7 +26,7 @@ $PREFIX = str_repeat("../", $DEPTH_DIFFERENCE);
 		<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet"
 				integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 		<link href="<?php echo $PREFIX; ?>css/bootstrap.min.css" rel="stylesheet">
-
+		<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-T8Gy5hrqNKT+hzMclPo118YTQO6cYprQmhrYwIiQ/3axmI1hQomh7Ud2hPOy8SP1" crossorigin="anonymous">
 		<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
 		<script src="https://code.jquery.com/jquery-2.2.4.min.js"
 				  integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
