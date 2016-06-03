cc8bbcdd5ab9a127b7de88949ca8209c6f585691
Anthony Williams <mylifeassurance@aol.com>
2016-06-03 14:24:29 -0600

    added github image

--- public_html/php/partials/footer.php
+++ public_html/php/partials/footer.php
@@ -2,7 +2,7 @@
 	<!-- Site footer -->
 	<footer>
 		<p>&copy; 2016 Williams DevOps, Inc.</p>
-		<img src="../../img/github-logo1.jpg" alt="GitHub Logo">
+		<img src="../../img/github-logo1.png" alt="GitHub Logo">
 	</footer>
 	<!-- /container -->
 </div>
