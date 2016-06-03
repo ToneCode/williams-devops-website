2ff7b0407ad42edda072d95c5917db26f7a09206
Anthony Williams <mylifeassurance@aol.com>
2016-06-03 14:19:18 -0600

    corrected composer

--- public_html/php/partials/footer.php
+++ public_html/php/partials/footer.php
@@ -2,6 +2,7 @@
 	<!-- Site footer -->
 	<footer>
 		<p>&copy; 2016 Williams DevOps, Inc.</p>
+		<img src="../../img/github-logo1.jpg" alt="GitHub Logo" class="footer-image">
 	</footer>
 	<!-- /container -->
 </div>
