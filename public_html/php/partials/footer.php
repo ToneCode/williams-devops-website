e7c42cfb85fc2855d504d10cf4f2a810cc2332ff
Anthony Williams <mylifeassurance@aol.com>
2016-06-03 18:27:45 -0600

    removed images

--- public_html/php/partials/footer.php
+++ public_html/php/partials/footer.php
@@ -2,8 +2,6 @@
 	<!-- Site footer -->
 	<footer>
 		<p>&copy; 2016 Williams DevOps, Inc.</p>
-		<div class="navbar-text pull-right">
-		<a href="#"><i class="fa fa facebook-square"></i></a></div>
 	</footer>
 	<!-- /container -->
 </div>
